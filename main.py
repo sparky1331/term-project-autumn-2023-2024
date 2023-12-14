3f202dc9f6e82ac51b16270647a6f2e328219990
sparky1331 <sergej_b@MacBook-Air-Sergej.local>
2023-12-14 19:30:10 +0300

    commit 3 14.12.2023

--- main.py
+++ main.py
@@ -5,5 +5,31 @@ import math
 FPS = 60
 
 TURQOISE = 0x40E0D0
+
+k = 0.2
+
+
 class Car:
-    def __init__(self, x, y, vx, vy):
+    def __init__(self, screen: pygame.Surface, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.vx = 0
+        self.vy = 0
+        self.points = 0
+        self.mass = 2
+        self.angle = 1
+        self.thrust = 0
+        self.ax = 0
+        self.ay = 0
+
+    def move(self):
+        """Перемещение машинки"""
+        self.dragx = k * self.vx
+        self.dragy = k * self.vy
+        self.ax = (self.thrust * math.cos(self.angle) - self.dragx) / self.mass
+        self.ay = (self.thrust * math.sin(self.angle) - self.dragy) / self.mass
+        self.vx += self.ax
+        self.vy += self.ay
+        self.x += self.vx
+        self.y += self.vy
